95c161719e90df8cbbf08eec4d52974787987927
cole <coleinglis@gmail.com>
2026-08-28 18:47:17 +0000

    Final

--- tasks.py
+++ tasks.py
@@ -4,3 +4,8 @@ print(task["hours"])
 def describe_task(task):
     print(task["name"],"-", task["hours"], "hours -", task["status"])
 describe_task(task)
+def calculate_pay(hours, rate):
+    total=hours*rate
+    return total
+pay=calculate_pay(12, 45.50)
+print("Pay for this task:", pay)
