5688a7c7e82a77b1ccb50c346f5be745ad9c315d
cole <coleinglis@gmail.com>
2026-08-28 10:12:12 -0700

    Final

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
+name = "Cole"
+age = 28
+hourly_rate = 45.50
+is_certified = True
+
+print(name, age, hourly_rate, is_certified)
+print(type(name), type(age), type(hourly_rate), type(is_certified))
+
+hours = 8
+rate = 45.50
+pay = hours * rate
+print("Pay:", pay)
+
+worker_name = input("Enter worker name: ")
+print("Hello,", worker_name)
 
