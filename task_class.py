4f6a1e65d52ac8f8b772c3f91e34e2584fc7fec1
cole <coleinglis@gmail.com>
2026-08-31 19:54:12 +0000

    Final

--- task_class.py
+++ task_class.py
@@ -6,5 +6,6 @@ class Task:
     def mark_complete(self):
         self.status="complete"
 task1=Task("Pour foundation", 12, "in progress")
+print(task1.name, task1.hours, task1.status)
 task1.mark_complete()
 print(task1.name, task1.hours, task1.status)
